bfcad48704b6e12a1120609931b66e6c0e16e59a
AmauryPreclin <amaury.preclin@thredup.com>
2020-05-14 15:32:41 +0200

    correct res and req bug, add "addDate" feature

--- routes/players.js
+++ routes/players.js
@@ -9,21 +9,32 @@ let players = [
     name: "Salah",
     club: "LFC",
     age: "27",
+    addDate: new Date().toLocaleString("en-US"),
+    updateDate: null,
   },
 ];
 
+var requestTime = function (req, res, next) {
+  req.requestTime = new Date().toLocaleString("en-US");
+  next();
+};
+
+router.use(requestTime);
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.json(players);
+  res.header("Content-Type", "application/json");
+  //res.json(players);
+  res.send(JSON.stringify(players, null, 4));
 });
 
 router.get("/:playerId", function (req, res, next) {
-  console.log(req);
   let playerFound = false;
   for (let index = 0; index < players.length; index++) {
     if (players[index].id === parseInt(req.params.playerId)) {
       playerFound = true;
-      res.json(players[index]);
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify(players[index], null, 4));
     }
   }
   if (!playerFound) res.status(404).send("player not found");
@@ -34,9 +45,11 @@ router.post("/", function (req, res, next) {
   const body = req.body;
   if (!body.name) res.status(400).send("bad request, name missing");
   let player = {};
-  player["id"] = id + 1;
+  player.id = id + 1;
   id++;
   lodash.merge(player, body);
+  player.addDate = new Date().toLocaleString("en-US");
+  player.updateDate = null;
   players = [...players, player];
   res.json(player);
 });
@@ -49,6 +62,7 @@ router.post("/:playerId", function (req, res, next) {
   let player = {};
   player.id = parseInt(params.playerId);
   lodash.merge(player, body);
+  player.addDate = new Date().toLocaleString("en-US");
   for (let index = 0; index < players.length; index++) {
     if (players[index].id === player.id) {
       playerFound = true;
@@ -59,29 +73,31 @@ router.post("/:playerId", function (req, res, next) {
   if (!playerFound) res.status(404).send("player not found");
 });
 
-router.put("/:playerName", function (res, req, next) {
-  const params = req.req.params;
-  const body = req.req.body;
+router.put("/:playerName", function (req, res, next) {
+  const params = req.params;
+  const body = req.body;
   let player = {};
   player.id = id + 1;
   id++;
   player.name = params.playerName;
   lodash.merge(player, body);
+  player.addDate = new Date().toLocaleString("en-US");
+  player.updateDate = null;
   players = [...players, player];
-  res.res.json(player);
+  res.json(player);
 });
 
-router.delete("/:playerId", function (res, req, next) {
+router.delete("/:playerId", function (req, res, next) {
   let playerFound = false;
   const playersCopy = [...players];
   for (let index = 0; index < playersCopy.length; index++) {
-    if (playersCopy[index].id === parseInt(req.req.params.playerId)) {
+    if (playersCopy[index].id === parseInt(req.params.playerId)) {
       playerFound = true;
       players.splice(index, 1);
-      res.res.json(playersCopy[index]);
+      res.json(playersCopy[index]);
     }
   }
-  if (!playerFound) res.res.status(404).send("player not found");
+  if (!playerFound) res.status(404).send("player not found");
 });
 
 module.exports = router;
